0c50e9c8800dbc63ddec69e9923ae859b2bf6785
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-25 23:38:10 +0600

    router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import './index.css';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<App/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
