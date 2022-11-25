e035fcc3aff12f711fdabcb385e19ce72971cd28
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-25 23:27:07 +0600

    react fragment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            posts: []
+        }
+    }
+
+    // handle username
+    usernameHandle = (event) => {
+        this.setState({ username: event.target.value});
+    }
+    // handle password
+    passwordHandle = (event) => {
+        this.setState({ password: event.target.value});
+    }
+
+    componentDidMount() {
+        // this.setState({ username: 'shosen@me.com' });
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => this.setState({ posts: data}));
+    }
+
     render() {
+        const {username, password, posts} = this.state;
+        
         return (
             <div className='container py-5'>
                 <div className='row'>
@@ -12,11 +41,22 @@ class Form extends Component {
                                     <h1 className="text-center mb-3">Login Form</h1>
                                     <div className="mb-3">
                                         <label htmlFor="email" className="form-label">Email address</label>
-                                        <input type="email" id="email" className="form-control" />
+                                        <input 
+                                            type="email" 
+                                            id="email" 
+                                            value={username} 
+                                            className="form-control" 
+                                            onChange={this.usernameHandle}    
+                                        />
                                     </div>
                                     <div className="mb-3">
                                         <label htmlFor="password" className="form-label">Password</label>
-                                        <input type="password" className="form-control" id="password" />
+                                        <input 
+                                            type="password" value={password} 
+                                            className="form-control" 
+                                            id="password" 
+                                            onChange={this.passwordHandle} 
+                                        />
                                     </div>
                             
                                     <button type="submit" className="btn btn-primary">Login</button>
@@ -24,7 +64,6 @@ class Form extends Component {
                                 </form>
                             </div>
                         </div>
-
                     </div>
                 </div>
                 
---
+++ src/components/MyFragment.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// wrapper component
+function MyFragment() {
+    return (
+        <React.Fragment>
+            <h3>Fragment Example</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas officia numquam magni explicabo nesciunt laboriosam dolorem recusandae expedita assumenda. Nam nobis illo corrupti hic aperiam ducimus reiciendis optio, unde saepe.</p>
+        </React.Fragment>
+    )
+}
+
+export default MyFragment;
