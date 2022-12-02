e1cfd387c1464985760177b05131f8457a865186
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-02 12:48:10 +0600

    article list fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
-import Form from './components/Form';
-import MyFragment from './components/MyFragment';
 
 function App() {
+  // articles
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/articles/", {
+        'method': 'GET',
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': 'Token 24b975f03c5a5ef923b42c74c8d333d47606b33c'
+        }
+
+      })
+    .then(response => response.json())
+    .then(response => setArticles(response))
+    .catch(error => console.log(error));
+
+  }, []) // [] one time call
+
   return (
     <div className="App">
-      <MyFragment/>
+      <h1>Blog App</h1>
+      {
+        articles.map((article) => {
+          return <div key={article.id}>
+            <h2 >{article.title}</h2>
+            <p>{article.description}</p>
+            <p>Author: {article.created_user.email }</p>
+            <p>Created At: {article.created_at }</p>
+
+
+          </div> 
+        }) 
+      }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './App';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import PageNotFound from './components/PageNotFound';
-
-// router components
-function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<App/>} />
-        <Route exact path='/*' element={<PageNotFound/>} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    <App />
   </React.StrictMode>
 );
