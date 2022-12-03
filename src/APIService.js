83b26cdfa2a72c9ca15c89cb9a5a7ea3ff5c290d
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-03 13:41:30 +0600

    api service auth endpoint fix

--- src/APIService.js
+++ src/APIService.js
@@ -33,7 +33,7 @@ export default class APIService {
     }
 
     static LoginUser(body) {
-        return fetch('http://127.0.0.1:8000/auth/', {
+        return fetch('http://127.0.0.1:8000/api/auth/', {
             'method':'POST',
             headers: {
                 'Content-Type':'application/json',
